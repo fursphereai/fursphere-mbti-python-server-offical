6a85238c7bfe8ae32ffd310503ea318907e8c55b
keyi li <keyili@keyis-MacBook-Pro.local>
2025-04-08 16:41:17 -0500

    Initial commit: Project files

--- my_project/app/server.py
+++ my_project/app/server.py
@@ -254,7 +254,11 @@ def check_signup():
         }), 500
 
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5001)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))  
+    app.run(host="0.0.0.0", port=port)
 
 
